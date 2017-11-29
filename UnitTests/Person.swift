d4be6c0fcd682c18567adb52ee58296b18fb8008
steve thompson <steven.thompson.ca@gmail.com>
2017-11-29 14:19:36 -0500

    make Person encodable for saving

--- UnitTests/Person.swift
+++ UnitTests/Person.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+
+
+class Person: NSCoding {
+  
+  // MARK: Encoding/Decoding
+  
+  struct Keys {
+    static let firstNameKey = "firstNameKey"
+    static let lastNameKey = "lastNameKey"
+  }
+  
+  func encode(with aCoder: NSCoder) {
+    aCoder.encode(self.firstName, forKey: Keys.firstNameKey)
+    aCoder.encode(self.lastName, forKey: Keys.lastNameKey)
+  }
+  
+  required convenience init(coder aDecoder: NSCoder) {
+    let fn = aDecoder.decodeObject(forKey: Keys.firstNameKey) as? String
+    let ln = aDecoder.decodeObject(forKey: Keys.lastNameKey) as? String
+    self.init(firstName: fn ?? "", lastName: ln ?? "")
+  }
+  
+  // MARK: Init
+  
+  init(firstName: String, lastName: String) {
+    self.firstName = firstName
+    self.lastName = lastName
+  }
+  
+  // MARK: Properties
+  
+  let firstName: String
+  let lastName: String
+  
+  var fullName: String {
+    get {
+      return "\(firstName) \(lastName)"
+    }
+  }
+
+}
